6dfb6a5187dbae108ac361759909d81974ec383f
YasserArafat <mdyasserarafat1@gmail.com>
2015-11-14 22:31:13 +0600

    [refs #2] - angular, gulp, browserify, livereload, sass integrated with oes ui. Initial structure ipplemented.

---
+++ oesui/app/scripts/controllers/WelcomeCtrl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var WelcomeCtrl = function($scope) {
+  $scope.testVar = 'Welcome to Online Examination System. Currently system is under construction.';
+};
+
+module.exports = WelcomeCtrl;
